043032b719bc5c6e4d7c65cf9e041a992513b96f
itsamitofficial37 <amitofficial37@gmail.com>
2023-12-26 12:11:14 +0530

    commit

---
+++ src/components/RestaurantCategory.js
@@ -0,0 +1,9 @@
+const RestaurantCategory = () => {
+  return (
+    <div>
+      <p>Recomended</p>
+    </div>
+  );
+};
+
+export default RestaurantCategory;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,7 @@
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
@@ -8,6 +9,11 @@ const RestaurantMenu = () => {
 
   const { name, cuisines, costForTwoMessage, areaName, avgRating } =
     resInfo?.cards[0]?.card?.card?.info || {};
+
+  const categories =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards || [];
+
+  console.log(categories);
 
   return resInfo === null ? (
     <Shimmer />
@@ -21,6 +27,11 @@ const RestaurantMenu = () => {
         <li>{areaName}</li>
         <li>{avgRating}</li>
       </ul>
+      <div>
+        {categories.map((c) => (
+          <RestaurantCategory key={c?.card?.info.id} data={c?.card?.info} />
+        ))}
+      </div>
     </div>
   );
 };
