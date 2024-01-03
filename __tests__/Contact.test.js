a35cea0fda69a62f2a246fb81634d03623a786bd
itsamitofficial37 <amitofficial37@gmail.com>
2024-01-03 11:45:03 +0530

    commit

--- __tests__/Contact.test.js
+++ __tests__/Contact.test.js
@@ -1,11 +1,56 @@
-
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Contact from "../src/components/Contact";
 
+// contact us heading 
 
 test("Should render Contact Us Page heading", () => {
   render(<Contact />);
   const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument()
+  expect(heading).toBeInTheDocument();
 });
+
+// submit button is there or not 
+
+test("Should button is there in contact component", ()=> {
+  render(<Contact/>);
+
+  // querying 
+  const button = screen.getByRole("button");
+
+  // Assertion 
+
+  expect(button).toBeInTheDocument();
+});
+
+// submit text 
+
+test("Should submit text is there in contact component", ()=> {
+  render(<Contact/>);
+
+  const submitText = screen.getByText("Submit");
+
+  expect(submitText).toBeInTheDocument();
+})
+
+// input text is there pr not 
+
+test("Should input is there in contact component", ()=> {
+  render(<Contact/>)
+
+  const textbox = screen.getAllByRole("textbox");
+  console.log(textbox[1]);
+
+  // expect().toBeInTheDocument();
+})
+
+// placeholder name check 
+
+test("Should placeholder have name  is there in contact component", ()=> {
+  render(<Contact/>)
+
+  const placeholder = screen.getByPlaceholderText("name");
+  
+
+  expect(placeholder).toBeInTheDocument();
+})
